7f8a9ae7b1074e06562c81695de4dac7bfc31bb6
Pheewww <umangsingh67899876@gmail.com>
2024-02-23 05:15:25 +0530

    More

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,14 +1,28 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-    facebookId: { type: String, unique: true },
+    facebookId: { type: String, unique: true, sparse: true },
     displayName: String,
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
     pageAccessTokens: [{
         pageId: String,
         accessToken: String,
         name: String,
     }]
 });
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
