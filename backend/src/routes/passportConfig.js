f37da3bd36bf3c628bb62d7a161939c2381985ad
Pheewww <umangsingh67899876@gmail.com>
2024-02-23 06:19:24 +0530

    Mongo Error model issue.

--- backend/src/routes/passportConfig.js
+++ backend/src/routes/passportConfig.js
@@ -69,11 +69,15 @@ const configurePassport = (passport) => {
         done(null, user.id);
     });
 
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => {
-            done(err, user);
-        });
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (error) {
+            done(error, null);
+        }
     });
+
 };
 
 
