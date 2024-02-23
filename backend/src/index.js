379e9767c2468447f266052d5d4f14c08e4f9959
Pheewww <umangsingh67899876@gmail.com>
2024-02-23 05:49:43 +0530

    Mongo Error.

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,13 @@ app.use(cors({
 
 
 
-mongoose.connect('process.env.MONGODB_URI', {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
-})
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error(err));
+try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+} catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1)
+}
 
 app.use(session({
     secret: 'process.env.EXPRESS_SESSION_SECRET',
