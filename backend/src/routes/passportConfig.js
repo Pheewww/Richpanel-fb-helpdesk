df769ee75288e16f278dc8fffe00744b2f44d06d
Pheewww <umangsingh67899876@gmail.com>
2024-02-23 06:11:08 +0530

    Mongo Error model issue.

--- backend/src/routes/passportConfig.js
+++ backend/src/routes/passportConfig.js
@@ -64,6 +64,16 @@ const configurePassport = (passport) => {
             done(error, null);
         }
     }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
 };
 
 
